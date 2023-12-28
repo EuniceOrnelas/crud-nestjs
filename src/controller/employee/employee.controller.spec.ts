391cde7a960551990ac675d9d02793857521a12c
Eunice <eunice.ornelas@2bcore.com>
2023-12-27 23:34:35 -0600

    update

--- src/controller/employee/employee.controller.spec.ts
+++ src/controller/employee/employee.controller.spec.ts
@@ -1,30 +1,85 @@
+// Importaciones necesarias para las pruebas unitarias
 import { Test, TestingModule } from '@nestjs/testing';
-import { EmployeeController } from './employee.controller';
-import { EmployeeService } from '../../service/employee/employee.service';
-import { getModelToken } from '@nestjs/mongoose';
-import { Employee } from '../../schema/employee.schema';
-/**
- * Pruebas unitarias para el controlador EmployeeController.
- */
+import { EmployeeController } from './employee.controller'; // Importa el controlador de empleados a probar
+import { EmployeeService } from '../../service/employee/employee.service'; // Importa el servicio de empleados
+import { CreateEmployeeDto } from '../../dto/create-employee.dto'; // Importa el DTO para crear empleados
+import { UpdateEmployeeDto } from '../../dto/update-employee.dto'; // Importa el DTO para actualizar empleados
+import mongoose, { Model } from 'mongoose'; // Importa Mongoose y el modelo empleado (Employee)
+import { Employee } from '../../schema/employee.schema'; // Importa el esquema empleado (Employee)
+
 describe('EmployeeController', () => {
-  let controller: EmployeeController;
-  const MockEmployeeService = {};
-  /**
-   * Antes de cada prueba, se crea un módulo de prueba y se obtiene una instancia del controlador.
-   */
+  let employeeController: EmployeeController; // Declara una variable para el controlador de empleados
+  let employeeService: EmployeeService; // Declara una variable para el servicio de empleados
+  let model: Model<Employee>; // Declara una variable para el modelo de empleados (Employee)
+
+  // Configura el entorno antes de cada prueba
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [EmployeeController],
-      providers: [EmployeeService, {provide: getModelToken(Employee.name), useValue:MockEmployeeService}]
-    }).compile();
+      controllers: [EmployeeController], // Define los controladores para las pruebas
+      providers: [EmployeeService], // Define los proveedores/servicios para las pruebas
+    }).compile(); // Compila el módulo de pruebas
 
-    controller = module.get<EmployeeController>(EmployeeController);
+    employeeController = module.get<EmployeeController>(EmployeeController); // Obtiene una instancia del controlador de empleados
+    employeeService = module.get<EmployeeService>(EmployeeService); // Obtiene una instancia del servicio de empleados
   });
 
-  /**
-   * Prueba para verificar que el controlador esté definido.
-   */
+  // Prueba básica para verificar si el controlador está definido
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(employeeController).toBeDefined(); // Verifica que el controlador de empleados esté definido
   });
+
+  // Bloque de pruebas para el método createEmployee del controlador
+  describe('createEmployee', () => {
+    // Prueba para verificar la creación de un nuevo empleado
+    it('should create a new employee', async () => {
+      // Define un objeto de ejemplo para simular la creación de un empleado
+      const mockCreateEmployeeDto: CreateEmployeeDto = {
+        name: "Markos",
+        employeeNumber: 224,
+        role: "developer",
+        gender: "Male",
+        age: 32
+      };
+
+      // Simula la creación de un empleado y devuelve el objeto simulado
+      jest.spyOn(model, 'create')
+        .mockImplementationOnce(() => Promise.resolve(mockCreateEmployeeDto));
+
+      // Ejecuta el método createEmployee del controlador con los datos simulados y espera la respuesta
+      const response = await employeeController.createEmployee({} as any, mockCreateEmployeeDto);
+
+      // Verifica que la respuesta coincida con el objeto esperado
+      expect(response).toMatchObject({
+        message: 'Employee has been created successfully',
+        newEmployee: mockCreateEmployeeDto,
+      });
+    });
+
+    // Prueba para verificar el manejo de errores al crear un empleado
+    it('should handle error while creating employee', async () => {
+      // Define un objeto de ejemplo para simular la creación de un empleado
+      const mockCreateEmployeeDto: CreateEmployeeDto = {
+        name: "Markos",
+        employeeNumber: 224,
+        role: "developer",
+        gender: "Male",
+        age: 32
+      };
+
+      // Simula un error al crear un empleado y devuelve un error
+      jest.spyOn(employeeService, 'createEmployee').mockRejectedValueOnce(new Error('Failed to create employee'));
+
+      // Ejecuta el método createEmployee del controlador con datos simulados y espera la respuesta de error
+      const response = await employeeController.createEmployee({} as any, mockCreateEmployeeDto);
+
+      // Verifica que la respuesta de error coincida con el objeto esperado
+      expect(response).toMatchObject({
+        statusCode: 400,
+        message: 'Error: Employee not created!',
+        error: 'Bad Request',
+      });
+    });
+  });
+
 });
+
--- src/service/employee/employee.service.ts
+++ src/service/employee/employee.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { CreateEmployeeDto } from 'src/dto/create-employee.dto';
 import { IEmployee } from 'src/interface/employee.interface';
@@ -7,6 +7,7 @@ import { UpdateEmployeeDto } from 'src/dto/update-employee.dto';
 
 @Injectable()
 export class EmployeeService {
+    private readonly logger = new Logger(EmployeeService.name);
     constructor(@InjectModel('Employee') private employeeModel: Model<IEmployee>) { }
 
     /**
@@ -15,8 +16,15 @@ export class EmployeeService {
      * @returns Una promesa que resuelve en un objeto de empleado creado.
      */
     async createEmployee(createEmployeeDto: CreateEmployeeDto): Promise<IEmployee> {
-        const newEmployee = await new this.employeeModel(createEmployeeDto);
-        return newEmployee.save();
+        try {
+            const newEmployee = await new this.employeeModel(createEmployeeDto);
+            const savedEmployee = await newEmployee.save();
+            this.logger.log(`Employee created: ${savedEmployee._id}`);
+            return savedEmployee;
+        } catch (error) {
+            this.logger.error(`Failed to create employee: ${error.message}`);
+            throw error;
+        }
     }
 
     /**
@@ -27,11 +35,17 @@ export class EmployeeService {
      * @throws NotFoundException si el empleado no se encuentra.
      */
     async updateEmployee(employeeId: string, updateEmployeeDto: UpdateEmployeeDto): Promise<IEmployee> {
-        const existingEmployee = await this.employeeModel.findByIdAndUpdate(employeeId, updateEmployeeDto, { new: true });
-        if (!existingEmployee) {
-            throw new NotFoundException(`Employee #${employeeId} not found`);
+        try {
+            const existingEmployee = await this.employeeModel.findByIdAndUpdate(employeeId, updateEmployeeDto, { new: true });
+            if (!existingEmployee) {
+                throw new NotFoundException(`Employee #${employeeId} not found`);
+            }
+            this.logger.log(`Employee updated: ${existingEmployee._id}`);
+            return existingEmployee;
+        } catch (error) {
+            this.logger.error(`Failed to update employee: ${error.message}`);
+            throw error;
         }
-        return existingEmployee;
     }
 
     /**
@@ -40,11 +54,17 @@ export class EmployeeService {
      * @throws NotFoundException si no se encuentran empleados.
      */
     async getAllEmployees(): Promise<IEmployee[]> {
-        const employeeData = await this.employeeModel.find();
-        if (!employeeData || employeeData.length === 0) {
-            throw new NotFoundException('Employees data not found!');
+        try {
+            const employeeData = await this.employeeModel.find();
+            if (!employeeData || employeeData.length === 0) {
+                throw new NotFoundException('Employees data not found!');
+            }
+            this.logger.log(`Retrieved all employees`);
+            return employeeData;
+        } catch (error) {
+            this.logger.error(`Failed to get all employees: ${error.message}`);
+            throw error;
         }
-        return employeeData;
     }
 
     /**
@@ -54,11 +74,17 @@ export class EmployeeService {
      * @throws NotFoundException si el empleado no se encuentra.
      */
     async getEmployee(employeeId: string): Promise<IEmployee> {
-        const existingEmployee = await this.employeeModel.findById(employeeId).exec();
-        if (!existingEmployee) {
-            throw new NotFoundException(`Employee #${employeeId} not found`);
+        try {
+            const existingEmployee = await this.employeeModel.findById(employeeId).exec();
+            if (!existingEmployee) {
+                throw new NotFoundException(`Employee #${employeeId} not found`);
+            }
+            this.logger.log(`Retrieved employee: ${existingEmployee._id}`);
+            return existingEmployee;
+        } catch (error) {
+            this.logger.error(`Failed to get employee: ${error.message}`);
+            throw error;
         }
-        return existingEmployee;
     }
 
     /**
@@ -68,10 +94,16 @@ export class EmployeeService {
      * @throws NotFoundException si el empleado no se encuentra.
      */
     async deleteEmployee(employeeId: string): Promise<IEmployee> {
-        const deletedEmployee = await this.employeeModel.findByIdAndDelete(employeeId);
-        if (!deletedEmployee) {
-            throw new NotFoundException(`Employee #${employeeId} not found`);
+        try {
+            const deletedEmployee = await this.employeeModel.findByIdAndDelete(employeeId);
+            if (!deletedEmployee) {
+                throw new NotFoundException(`Employee #${employeeId} not found`);
+            }
+            this.logger.log(`Employee deleted: ${deletedEmployee._id}`);
+            return deletedEmployee;
+        } catch (error) {
+            this.logger.error(`Failed to delete employee: ${error.message}`);
+            throw error;
         }
-        return deletedEmployee;
     }
 }
